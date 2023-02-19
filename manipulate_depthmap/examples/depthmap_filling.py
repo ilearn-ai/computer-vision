db268a001adc49abf3031021727fb4db4c444183
ailoher <aitor.lopez.hernandez@gmail.com>
2023-02-19 23:07:28 +0100

    Add operational inpaint method. Add operational radar plot, pending to make it work for many methods. Rename plot_utilities by plottings. Create plotting_utilities folder where to store radar plot utilities. Refactor methods to classes.

--- manipulate_depthmap/examples/depthmap_filling.py
+++ manipulate_depthmap/examples/depthmap_filling.py
@@ -1,20 +1,58 @@
 from pathlib import Path
 
 import pandas as pd
+import cv2
 
-from manipulate_depthmap.hole_filling_methods.interpolation_methods import (
-    fill_holes_in_sparse_map_using_interpolation,
+from manipulate_depthmap.hole_filling_methods.inpainting_methods import (
+    InpaintingMethod,
 )
-from manipulate_depthmap.utilities.plot_utilities import (
+from manipulate_depthmap.hole_filling_methods.interpolation_methods import (
+    InterpolationMethod,
+)
+from manipulate_depthmap.utilities.plottings import (
     plot_depth_map_from_img_dataframe,
 )
 
+image_name = "Rosemary"
+
 sparse_map_url = (
-    Path(__file__).parent.parent / "source_images" / "Rosemary" / "sparse_depth.dat"
+    Path(__file__).parent.parent
+    / "source_images"
+    / f"{image_name}"
+    / "sparse_depth.dat"
 )
 sparse_map = pd.read_csv(sparse_map_url, header=None)
 plot_depth_map_from_img_dataframe(sparse_map)
 
+ground_truth_url = (
+    Path(__file__).parent.parent / "source_images" / f"{image_name}" / "gt_depth.dat"
+)
+ground_truth = pd.read_csv(ground_truth_url, header=None)
+plot_depth_map_from_img_dataframe(ground_truth)
+
+source_img_url = (
+    Path(__file__).parent.parent / "source_images" / f"{image_name}" / "image.png"
+)
+source_img = cv2.imread(str(source_img_url))
+
+# Filling though interpolation
 method = "linear"
-filled_depth_map = fill_holes_in_sparse_map_using_interpolation(sparse_map, method)
-plot_depth_map_from_img_dataframe(filled_depth_map)
+interpolation_method = InterpolationMethod(interpolation_type=method)
+filled_depth_map_interpol = interpolation_method.fill_holes(depth_map=sparse_map)
+interpolation_method.calculate_metrics(
+    depth_map=filled_depth_map_interpol, ground_truth=ground_truth
+)
+plot_depth_map_from_img_dataframe(filled_depth_map_interpol)
+interpolation_method.plot_metrics()
+
+# Filling through inpainting
+inpaint_radius = 5
+inpainting_method = InpaintingMethod(inpaint_radius=inpaint_radius)
+filled_depth_map_inpaint = inpainting_method.fill_holes(depth_map=sparse_map)
+inpainting_method.calculate_metrics(
+    depth_map=filled_depth_map_inpaint, ground_truth=ground_truth
+)
+plot_depth_map_from_img_dataframe(filled_depth_map_inpaint)
+inpainting_method.plot_metrics()
+
+# Filling using source image with CNN
---
+++ manipulate_depthmap/hole_filling_methods/fill_depth_map_parent.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from typing import Union
+from manipulate_depthmap.utilities.plottings import (
+    plot_radar_chart,
+)
+
+
+class FillDepthMap:
+    def __init__(self, name):
+        self.name = name
+        self.mse = np.nan
+        self.rmse = np.nan
+        self.mae = np.nan
+
+    def fill_holes(self, depth_map):
+        raise NotImplementedError("Method not implemented")
+
+    def validate_input_depth_map(self, depth_map):
+        if isinstance(depth_map, pd.DataFrame):
+            depth_map = (
+                depth_map.to_numpy()
+            )  # Change depth map to numpy format to be used by cv2.inpaint method
+        elif not isinstance(depth_map, np.ndarray):
+            raise TypeError("depth_map must be a pandas DataFrame of numpy array")
+
+        if depth_map.dtype != np.uint8:
+            depth_map = depth_map.astype(
+                np.uint8
+            )  # Change depth map numpy format to uint8 to make it compatible with cv2.inpaint method
+
+        if len(depth_map.shape) != 2:
+            raise ValueError("depth_map must have 2 dimensions")
+
+        return depth_map
+
+    def calculate_metrics(
+        self, depth_map: Union[pd.DataFrame, np.ndarray], ground_truth: pd.DataFrame
+    ) -> None:
+        depth_map = self.validate_input_depth_map(depth_map)
+        ground_truth = ground_truth.values
+        self.mse = np.mean(np.square(ground_truth - depth_map))
+        self.rmse = np.sqrt(self.mse)
+        self.mae = np.mean(np.abs(ground_truth - depth_map))
+
+    def plot_metrics(self) -> None:
+        kpis = [
+            "MSE (Mean square error)",
+            "RMSE (Root mean square error)",
+            "MAE (Mean absolute error)",
+        ]
+        scores = [self.mse, self.rmse, self.mae]
+        method = self.name
+        plot_radar_chart(kpis, scores, method)
